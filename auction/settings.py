ce210c5daa29ede05e2ca965f674c80b507a90cb
sauravpanchal <saurav.panchal.08@gmail.com>
2022-09-01 23:57:57 +0530

    setting up .env

--- auction/settings.py
+++ auction/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
+from dotenv import load_dotenv
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&w62hkx*-j#^dtqvcp&@&%pm0^-28vr2a7a!fz&0yd09di3s92'
+load_dotenv()
+SECRET_KEY = os.getenv("KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
